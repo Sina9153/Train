8835e79bbfc2b37a4d80b355313f073593e5b634
Sina <sina91@example.com>
2026-08-01 18:08:29 +0330

    a

---
+++ Train_16.py
@@ -0,0 +1,9 @@
+import os
+
+a=os.getcwd()
+
+def change_dir(get_dir):
+
+    os.chdir(get_dir)
+
+change_dir(get_dir=r"D:\a")
---
+++ Train_18_excel.py
@@ -0,0 +1,8 @@
+import pyexcel
+
+excel_path=r"D:\Python\Sina\Train\Train_18_excel.py"
+
+def get_sheet(file_name):
+    pass
+
+get_sheet(file_name=excel_path)
