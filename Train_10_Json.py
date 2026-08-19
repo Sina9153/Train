d6237c358a98a7034f1402878c536d6d87de3907
Sina <sina91@example.com>
2026-08-19 17:43:38 +0330

    a

--- Train_10_Json.py
+++ Train_10_Json.py
@@ -1,6 +1,26 @@
 import json
-users={1:{"name":"Ali","age":"20"},2:{"name":"Mohsen","age:":"22"}}
-with open("users.json","w") as file:
-    json.dump(users,file)
-with open("users.json","r") as file:
-    json.load(file)
+
+users = {1: {"name": "Ali", "age": "20"},2: {"name": "Mohsen", "age": "22"}}
+
+def save(users):
+    with open("users.json", "w") as file:
+        json.dump(users, file, indent=4)
+
+def load_users():
+    with open("users.json", "r") as file:
+        return json.load(file)
+
+def add():
+    users = load_users()
+    users["3"] = {"name": "Ahmad","age": "14"}
+    save(users)
+
+save(users)
+
+users = load_users()
+print(users)
+
+add()
+
+users = load_users()
+print(users)
